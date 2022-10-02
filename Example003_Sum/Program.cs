c08cebd19b867728604e2bd2bca7094b935b66c6
ekaterina-chusova <ekaterina_ch_@mail.ru>
2022-10-02 14:05:11 +0500

    add random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); //1 2 3...9
+Console.WriteLine($"Первое число равно: {numberA}");
+int numberB = new Random().Next(1,10);
+Console.WriteLine($"Второе число равно: {numberB}");
 int result = numberA + numberB;
 Console.WriteLine(result);
